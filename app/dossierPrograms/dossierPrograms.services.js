375e4b198750923a7eb9c465ce45ba8a4456465f
Eric Mourin <ericmourin@hotmail.com>
2017-05-30 14:50:41 +0200

    query fields changed to displayFormName,displayDescription

--- app/dossierPrograms/dossierPrograms.services.js
+++ app/dossierPrograms/dossierPrograms.services.js
@@ -5,9 +5,9 @@
 
 var qryPrograms = dhisUrl + 'programs.json?fields=id,displayName,programStages[id]&paging=false';
 
-var qryProgramStageSections = dhisUrl + 'programStages/:programStageId.json?fields=programStageSections[id,displayName,programStageDataElements[dataElement[displayName,formName,description,valueType,optionSetValue,optionSet[options[displayName]]]]],programStageDataElements[dataElement[displayName,formName,description,valueType,optionSetValue,optionSet[options[displayName]]]]&paging=false';
+var qryProgramStageSections = dhisUrl + 'programStages/:programStageId.json?fields=programStageSections[id,displayName,programStageDataElements[dataElement[displayName,displayFormName,displayDescription,valueType,optionSetValue,optionSet[options[displayName]]]]],programStageDataElements[dataElement[displayName,displayFormName,displayDescription,valueType,optionSetValue,optionSet[options[displayName]]]]&paging=false';
 
-var qryProgramIndicators = dhisUrl + 'programs/:programId.json?fields=programIndicators[displayName,description,expression,filter]';
+var qryProgramIndicators = dhisUrl + 'programs/:programId.json?fields=programIndicators[displayName,displayDescription,expression,filter]';
 
 var qryProgramIndicatorExpressions = dhisUrl + 'programIndicators/expression/description'
 var qryProgramIndicatorFilters = dhisUrl + 'programIndicators/filter/description'
