478c5700de908b983cf20992484be797bfcdee89
Sultanahamer <sultanahamer@gmail.com>
2017-09-26 10:58:01 +0530

    [Sultan] - Added browser sync to serve the app,  watch to make development easier

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,52 @@
 var gulp = require('gulp');
-var zip = require('gulp-zip');
+  zip = require('gulp-zip');
+  browserSync = require('browser-sync').create(),
+  gulpsync    = require('gulp-sync')(gulp),
+  proxy       = require('http-proxy-middleware'),
 
-gulp.task('package', function() {
-    return gulp.src(['./**/*', '!doc/**', '!doc'])
-        .pipe(zip('HMIS_Dictionary.zip'))
-        .pipe(gulp.dest('target'));
+APP = {
+  src: {
+    root: ".",
+    all: "app/**/*.*",
+  },
+};
+
+TASKS = {
+  watchSrc: '_watchSrc',
+  cleanTemp: '_cleanTemp',
+  clean: 'clean',
+  serve: 'serve',
+  copyResourcesToTemp: '_copyResourcesToTemp',
+  setUpTemp: '_setUpTemp',
+  reload: '_reload',
+  webpackTest: '_webpackTest',
+  pack: 'pack',
+};
+
+gulp.task(TASKS.reload, function() {
+  browserSync.reload();
 });
+
+gulp.task(TASKS.watchSrc, function() {
+  gulp.watch([APP.src.all], gulpsync.sync([TASKS.reload]));
+});
+
+gulp.task(TASKS.serve, function() {
+  browserSync.init({
+    port: 8000,
+    server: {
+      port: 8000,
+      baseDir: APP.src.root,
+      middleware: [proxy(['/api', '/dhis-web-commons', '/icons'], {target: 'http://localhost:8080'})]
+    }
+
+  });
+});
+
+gulp.task(TASKS.pack, function() {
+  return gulp.src(['./**/*', '!doc/**', '!doc'])
+    .pipe(zip('HMIS_Dictionary.zip'))
+    .pipe(gulp.dest('target'));
+});
+
+gulp.task('default', [TASKS.serve, TASKS.watchSrc]);
