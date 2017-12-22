8c2d8732cc79889f8b4f84591a341fa487953c6a
srikantk <srikantk@INSrikantk.local>
2017-12-22 15:37:10 +0530

    [Srikanth/Kaustav] removed datastore used config inplace of that

---
+++ app/config/config.js
@@ -0,0 +1,10 @@
+appModule.factory('Config', [function() {
+		return {
+			userGroupId: 'mTJVJfBK6RB',
+			organisationGroupSetId:'QSiGnQwGi92',
+			blackListDataSetsIds:['YoJjavJ432r','wcCbcAKbP'],
+			blackListIndicatorGroupIds:[]
+			
+		}
+	}
+]);
